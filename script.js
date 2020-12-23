427825edc5d4a0906af48b745d352593da55b1bd
charles rainey <charles.rainey@gmail.com>
2020-12-22 19:08:11 -0500

    new pull

--- script.js
+++ script.js
@@ -348,7 +348,7 @@ function getMovie() {
     }).then(function (response) {
 
         console.log(response)
-        var movieDiv = $("<div class='movie'>");
+        var movieDiv = $("<div class='moviesBtn'>");
     })
 
 };
